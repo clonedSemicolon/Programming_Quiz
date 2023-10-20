f31ac25e95f4e97ce661e5c4885cd658e500e135
Avishak-cb <avishak@daricomma.com>
2023-10-21 02:26:14 +0600

    Solved some issues from real-device.

--- app/src/main/java/com/example/programming_quiz_app/ui/screens/quiz/question/QuestionFragment.kt
+++ app/src/main/java/com/example/programming_quiz_app/ui/screens/quiz/question/QuestionFragment.kt
@@ -3,6 +3,7 @@ package com.example.programming_quiz_app.ui.screens.quiz.question
 import Timer
 import android.annotation.SuppressLint
 import android.graphics.Color
+import android.graphics.drawable.Drawable
 import android.graphics.drawable.GradientDrawable
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
@@ -14,7 +15,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
 import com.example.programming_quiz_app.R
 import com.example.programming_quiz_app.data.model.Quiz
 import com.example.programming_quiz_app.databinding.FragmentQuestionBinding
@@ -67,10 +72,33 @@ class QuestionFragment : Fragment() {
         "Score:${quiz.score}".also { binding.questionScore.text = it }
         quiz.questionImageUrl?.let {url->
             binding.questionImage.visibility = View.VISIBLE
+            binding.glideProgress.visibility = View.VISIBLE
             context?.let {
                 Glide.with(it).load(url)
                     .error(R.drawable.error_icon)
-                    .placeholder(R.drawable.loading)
+                    .listener(object : RequestListener<Drawable>{
+                        override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            binding.glideProgress.visibility = View.INVISIBLE
+                            return false
+                        }
+
+                        override fun onResourceReady(
+                            resource: Drawable?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            dataSource: DataSource?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            binding.glideProgress.visibility = View.INVISIBLE
+                            return false
+                        }
+
+                    })
                     .into(binding.questionImage)
             }
         }
